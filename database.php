49e66fdc65b71584be1e6c60faafb6a94193beb5
sadeep peiris <sadeep8@live.com>
2020-09-16 10:35:41 +0530

    first commit

---
+++ database.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$host="localhost";
+$user ="root";
+$pw="";
+
+$conn = mysqli_connect($host,$user,$pw);
+
+if($conn){
+
+    echo "connected";
+
+}else{
+
+    die('error in db'.mysqli_error());
+}
+
+echo "hello";
+?>
+    
+</body>
+</html>
